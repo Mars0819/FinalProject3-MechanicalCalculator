7ee76108ff01abb06c016b535b34eca64fba7a7d
Mars0819 <umaralfa46@gmail.com>
2022-11-28 20:43:10 +0700

    Mechanical Calculator

---
+++ app/src/main/java/com/hanifa/mechanicalcalculatorfinpro3/MainActivity.java
@@ -0,0 +1,222 @@
+package com.hanifa.mechanicalcalculatorfinpro3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnPersen,btnTambah,btnKurang,btnKali,btnBagi, btnHasil, btnHapus, btnTitik,btnBackspace;
+    TextView angkaMasuk, angkaKeluar;
+    String process;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        angkaMasuk = findViewById(R.id.angkaMasuk);
+        angkaKeluar = findViewById(R.id.angkaKeluar);
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
+        btnTambah = findViewById(R.id.btnTambah);
+        btnKurang = findViewById(R.id.btnKurang);
+        btnBagi = findViewById(R.id.btnBagi);
+        btnKali = findViewById(R.id.btnKali);
+
+        btnHasil = findViewById(R.id.btnHasil);
+
+        btnHapus = findViewById(R.id.btnHapus);
+        btnTitik = findViewById(R.id.btnTitik);
+        btnPersen = findViewById(R.id.btnPersen);
+        btnBackspace = findViewById(R.id.btnBackspace);
+
+
+
+        btnHapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angkaMasuk.setText("");
+                angkaKeluar.setText("");
+            }
+        });
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "0");
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "9");
+            }
+        });
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "+");
+            }
+        });
+
+        btnKurang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "-");
+            }
+        });
+
+        btnKali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "x");
+            }
+        });
+
+        btnBagi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "/");
+            }
+        });
+
+        btnTitik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + ".");
+            }
+        });
+
+        btnPersen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+                angkaMasuk.setText(process + "%");
+            }
+        });
+
+        btnBackspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word = angkaMasuk.getText().toString();
+                int input = word.length();
+                if (input > 0){
+                    angkaMasuk.setText(word.substring(0, input - 1));
+                }
+            }
+        });
+
+        btnHasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = angkaMasuk.getText().toString();
+
+                process = process.replaceAll("x","*");
+                process = process.replaceAll("%","/100");
+                process = process.replaceAll("/","/");
+
+                Context rhino = Context.enter();
+                rhino.setOptimizationLevel(-1);
+                String finalResult = "";
+
+                try {
+                    Scriptable scriptable = rhino.initSafeStandardObjects();
+                    finalResult = rhino.evaluateString(scriptable,process,"javascript",1,null).toString();
+                }catch (Exception e){
+                    finalResult="0";
+                }
+                angkaKeluar.setText(finalResult);
+            }
+        });
+    }
+}
